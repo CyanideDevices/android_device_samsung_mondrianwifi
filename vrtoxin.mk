bc5c9e26402291298fadf63c3ca8036cb46d9b25
rogersb11 <brettrogers11@gmail.com>
2016-06-21 16:02:53 -0400

    Cyanide

--- vrtoxin.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
-
-# Inherit some common VRTOXIN stuff.
-$(call inherit-product, vendor/vrtoxin/config/common_full_tablet_wifionly.mk)
-
-PRODUCT_DEVICE := mondrianwifi
-PRODUCT_NAME := vrtoxin_mondrianwifi
